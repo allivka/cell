7b35d37cd31e16d9da8e86b71eae608c50711ca7
allivka <allivka1920@gmail.com>
2026-08-25 22:13:54 +0300

    experimenting for now

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::env::args;
+use std::io::{stdin, stdout, Write, stderr};
+use std::process::{Command};
+
+fn main(){
+
+    loop {
+        if let Err(e) = stdout().write(b"-> ") {
+            stderr().write_fmt(format_args!("{:?}\n", e));
+            continue;
+        };
+
+        if let Err(e) = stdout().flush() {
+            stderr().write_fmt(format_args!("{:?}\n", e));
+            continue;
+        };
+
+        let mut input = String::new();
+
+        if let Err(e) = stdin().read_line(&mut input) {
+            stderr().write_fmt(format_args!("{:?}\n", e));
+            continue;
+        };
+
+        let parts = input.trim().split_whitespace().collect::<Vec<_>>();
+        let command = parts[0];
+
+        match command {
+            "exit" => break,
+            _ => ()
+        }
+
+        let mut child = match Command::new(command).args(&parts[1..]).spawn() {
+            Ok(child) => child,
+            Err(e) => {
+                stderr().write_fmt(format_args!("{:?}\n", e));
+            continue;
+            },
+        };
+
+        if let Err(e) = child.wait() {
+            stderr().write_fmt(format_args!("{:?}\n", e));
+        };
+    }
+
+}
